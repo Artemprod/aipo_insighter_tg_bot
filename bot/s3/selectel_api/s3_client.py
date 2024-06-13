d9df90afb6340fef124dc400ac4fbf0f962c9187
spike <azamtoiri@gmail.com>
2024-06-13 20:13:45 +0300

    get new 3_client

--- bot/s3/selectel_api/s3_client.py
+++ bot/s3/selectel_api/s3_client.py
@@ -1,42 +1,64 @@
 import asyncio
+import uuid
 from contextlib import asynccontextmanager
+from pathlib import Path
 
 import aiofiles
+from aiobotocore.config import AioConfig
 from aiobotocore.session import get_session
 from botocore.exceptions import ClientError
-from environs import Env
 from types_aiobotocore_s3.client import S3Client as S3ClientType
 
-from bot.s3.selectel_api.StorageClient import SelStorage
+from container import settings
 
 
 class S3Client:
+    """
+    S3 client
+    """
+
     def __init__(
             self,
             access_key: str,
             secret_key: str,
             endpoint_url: str,
             bucket_name: str,
-            sel_client: SelStorage,
     ):
         self.config = {
-            "aws_access_key_id": access_key,
-            "aws_secret_access_key": secret_key,
-            "endpoint_url": endpoint_url,
+            "aws_access_key_id": access_key,  # access key from s3 storage [Required]
+            "aws_secret_access_key": secret_key,  # secret key from s3 storage [Required]
+            "endpoint_url": endpoint_url,  # endpoint for your s3 storage for selectel [https://s3.storage.selcloud.ru]
+            "region_name": 'ru-1',  # region name default [ru-1] [Optional]
+            "config": AioConfig(s3={'addressing_style': 'virtual'})  # [Optional]
         }
-        self.bucket_name = bucket_name
-        self.session = get_session()
-        self.sel_client = sel_client
+        self.bucket_name = bucket_name  # bucket name [Get from storage]
+        self.session = get_session()  # current session [async contextmanager]
 
     @asynccontextmanager
     async def get_client(self) -> S3ClientType:
+        """
+        This is an asynchronous context manager that creates and yields an S3 client.
+        The client is created using the current session and the configuration parameters
+        defined during the initialization of the S3Client class.
+
+        The client is automatically closed when exiting the 'with' block.
+
+        :return: An instance of S3ClientType which represents the S3 client.
+        """
         async with self.session.create_client("s3", **self.config) as client:
             yield client
 
     async def upload_file(self, file_path: str):
-        object_name = file_path.split("/")[-1]
+        """
+        This method uploads a file to the S3 bucket.
+        :param file_path: The path to the file to be uploaded.
+        :return: str file name in bucket
+        """
+        # Формируем уникальный id для названия файла и добавляем формат файла
+        object_name = f"{uuid.uuid5(uuid.NAMESPACE_DNS, Path(file_path).name)}.{Path(file_path).name.split('.')[-1]}"
         try:
             async with self.get_client() as client:
+                client: S3ClientType
                 async with aiofiles.open(file_path, "rb") as file:
                     await client.put_object(
                         Bucket=self.bucket_name,
@@ -44,20 +66,34 @@ class S3Client:
                         Body=await file.read(),
                     )
                 print(f"File {object_name} uploaded to {self.bucket_name}")
+                return object_name
         except ClientError as e:
             print(f"Error uploading file: {e}")
 
     async def delete_file(self, object_name: str):
+        """
+        This method deletes a file from the S3 bucket.
+        :param object_name: name of the object to be deleted from the bucket.
+        :return: None
+        """
         try:
             async with self.get_client() as client:
+                client: S3ClientType
                 await client.delete_object(Bucket=self.bucket_name, Key=object_name)
                 print(f"File {object_name} deleted from {self.bucket_name}")
         except ClientError as e:
             print(f"Error deleting file: {e}")
 
     async def download_file(self, object_name: str, destination_path: str):
+        """
+        This method downloads a file from the S3 bucket to the specified destination path.
+        :param object_name: object name to be downloaded from the bucket.
+        :param destination_path: path where the file will be downloaded.
+        :return: None
+        """
         try:
             async with self.get_client() as client:
+                client: S3ClientType
                 response = await client.get_object(Bucket=self.bucket_name, Key=object_name)
                 data = await response["Body"].read()
                 async with aiofiles.open(destination_path, "wb") as file:
@@ -66,9 +102,14 @@ class S3Client:
         except ClientError as e:
             print(f"Error downloading file: {e}")
 
-    async def get_all_object(self):
+    async def get_all_object(self) -> list[str]:
+        """
+        This method lists all objects in the S3 bucket. For each object, it returns the key.
+        :return: List of object keys in the bucket.
+        """
         try:
             async with self.get_client() as client:
+                client: S3ClientType
                 response = await client.list_objects_v2(Bucket=self.bucket_name)
                 if 'Contents' in response:
                     return [obj['Key'] for obj in response['Contents']]
@@ -78,48 +119,74 @@ class S3Client:
             return []
 
     async def get_bucket_access_control_list(self):
+        """
+        This method gets the access control list (ACL) for the S3 bucket.
+        Don't use this method in this project
+        :return: s3 bucket ACL
+        """
         try:
             async with self.get_client() as client:
+                client: S3ClientType
                 response = await client.get_bucket_acl(Bucket=self.bucket_name)
                 return response
         except ClientError as e:
             print(f"Error getting bucket ACL: {e}")
-            return None
 
-    async def get_object_link(self, Key: str):
+    async def generate_presigned_url(self, key: str, ExpiresIn: int = 1488) -> str:
+        """
+        This method generates a presigned URL for the object with the given key.
+        :param ExpiresIn: the time of link will be available (sec)
+        :param key: Object name for which the presigned URL is to be generated.
+        :return: str: Presigned URL for the object. [Expires in 1488 seconds default]
+        """
         try:
-            container_domen = await self.sel_client.get_pubdomains()
-            return f"https://{container_domen[0].get('uuid')}.selstorage.ru/{Key}"
-        except ClientError as e:
-            print(f"Error getting object link: {e}")
-            return None
+            async with self.get_client() as client:
+                client: S3ClientType
+                response = await client.generate_presigned_url(
+                    'get_object',
+                    Params={'Bucket': self.bucket_name, 'Key': key},
+                    ExpiresIn=ExpiresIn
+                )
+                return response
+        except Exception as err:
+            print(err)
+
+    async def get_object_data(self, key) -> dict:
+        """
+        Get information of object, like when was created and how size of the object
+        :param key: Object Name
+        :return: dict
+        """
+        try:
+            async with self.get_client() as client:
+                client: S3ClientType
+                response = await client.head_object(Bucket=self.bucket_name, Key=key)
+                return response
+        except Exception as err:
+            print(err)
 
 
 async def main():
-    env = Env()
-    env.read_env('.env')
-
-    username = env('SELECTEL_USERNAME')
-    password = env('SELECTEL_PASSWORD')
-    account_id = env('SELECTEL_ACCOUNT_ID')
-    project_name = 'My First Project'
-    bucket_name = 'public-insighter-1'
-
-    sel_client = SelStorage(
-        account_id=account_id,
-        project_name=project_name,
-        username=username,
-        password=password,
-        bucket_name=bucket_name,
-    )
+    # Example usage:
     s3_client = S3Client(
-        access_key=env("S3_ACCESS_KEY"),
-        secret_key=env("S3_SECRET_KEY"),
-        endpoint_url="https://s3.storage.selcloud.ru",
-        bucket_name="public-insighter-1",
-        sel_client=sel_client,
+        access_key=settings.selectel.access_key,
+        secret_key=settings.selectel.secret_key,
+        endpoint_url=settings.selectel.endpoint_url,
+        bucket_name=settings.selectel.bucket_name,
     )
-    print(await s3_client.get_object_link("posting-label-58515541-0006-2.pdf"))
+    obj = await s3_client.upload_file(
+        r"C:\Users\artem\OneDrive\Рабочий стол\Тестовые данные\#33. Операции над множествами, сравнение множеств _ Python для начинающих.mp4")
+    print(obj)
+    # uploading file to s3 storage
+    # list_1 = await s3_client.get_all_object()
+    # print(list_1)
+    # name_ob = list_1[0]
+    # await s3_client.delete_file(object_name=name_ob)
+    # object = await s3_client.upload_file(object_name=name_ob)
+    key = await s3_client.generate_presigned_url(key=obj)
+    print(key)
+    # print(key)
+    # await s3_client.download_file(object_name="#33. Операции над множествами, сравнение множеств _ Python для начинающих.mp4",destination_path=r"D:\projects\AIPO_V2\insighter_worker\downloaded_file.mp4")
 
 
 if __name__ == "__main__":
